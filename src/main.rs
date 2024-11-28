508069554f3a5b81c99995e1dfa17f873b488d27
Charles Thacher <crichardthacher@gmail.com>
2024-11-28 00:07:51 -0500

    added config struct

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,11 @@ fn main() {
     println!("With text:\n{contents}");
 }
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn parse_config(args: &[String]) -> (&str, str) {
     let query: = &args[1];
     let file_path = &args[2];

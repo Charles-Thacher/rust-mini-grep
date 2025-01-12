157c03c34b6cb8b3eeec43f8595a5e04f5fc0477
Charles Thacher <crichardthacher@gmail.com>
2025-01-12 17:30:15 -0500

    err handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
-use std::error::Error;
 
 fn main() {
+    // --snip--
+
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
@@ -11,16 +13,13 @@ fn main() {
         process::exit(1);
     });
 
-
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
-    
-/*     let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}"); */
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
